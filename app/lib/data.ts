0257630f911caab336a8f29e47727870f1595970
Jacek Gizinski <jacektrg+github@gmail.com>
2024-05-12 22:04:56 +0200

    Setting up APi providers data to share between components.

---
+++ app/lib/data.ts
@@ -0,0 +1,28 @@
+import { SourcesData } from './types';
+
+export const sourcesData: SourcesData = [
+	{
+		name: 'spotify',
+		color: 'green-600',
+		url: 'https://open.spotify.com/search/',
+		urlOption: '/tracks',
+	},
+	{
+		name: 'apple-music',
+		color: 'red-500',
+		url: 'https://music.apple.com/us/search?term=',
+		urlOption: '',
+	},
+	{
+		name: 'youtube',
+		color: 'red-700',
+		url: 'https://www.youtube.com/results?search_query=',
+		urlOption: '',
+	},
+	{
+		name: 'youtube-music',
+		color: 'red-600',
+		url: 'https://music.youtube.com/search?q=',
+		urlOption: '',
+	},
+];

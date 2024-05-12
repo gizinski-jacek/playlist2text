12a048ecef10d6dce0eb7baaab2dba2a4db714dd
Jacek Gizinski <jacektrg+github@gmail.com>
2024-05-12 22:05:49 +0200

    Adding Spotify's API response interface.

---
+++ app/lib/types.ts
@@ -0,0 +1,153 @@
+export type SourcesData = {
+	name: string;
+	color: string;
+	url: string;
+	urlOption: string;
+}[];
+
+export interface SpotifyPlaylistsResponse {
+	collaborative: false;
+	description: string;
+	external_urls: {
+		spotify: string;
+	};
+	followers: {
+		href: string;
+		total: number;
+	};
+	href: string;
+	id: string;
+	images: {
+		url: string;
+		height: number;
+		width: number;
+	}[];
+	name: string;
+	owner: {
+		external_urls: {
+			spotify: string;
+		};
+		followers: {
+			href: string;
+			total: number;
+		};
+		href: string;
+		id: string;
+		type: string;
+		uri: string;
+		display_name: string;
+	};
+	public: false;
+	snapshot_id: string;
+	tracks: {
+		href: string;
+		limit: number;
+		next: string;
+		offset: number;
+		previous: string;
+		total: number;
+		items: {
+			added_at: string;
+			added_by: {
+				external_urls: {
+					spotify: string;
+				};
+				followers: {
+					href: string;
+					total: number;
+				};
+				href: string;
+				id: string;
+				type: string;
+				uri: string;
+			};
+			is_local: false;
+			track: {
+				album: {
+					album_type: string;
+					total_tracks: 9;
+					available_markets: string[];
+					external_urls: {
+						spotify: string;
+					};
+					href: string;
+					id: string;
+					images: {
+						url: string;
+						height: number;
+						width: number;
+					}[];
+					name: string;
+					release_date: string;
+					release_date_precision: string;
+					restrictions: {
+						reason: string;
+					};
+					type: string;
+					uri: string;
+					artists: {
+						external_urls: {
+							spotify: string;
+						};
+						href: string;
+						id: string;
+						name: string;
+						type: string;
+						uri: string;
+					}[];
+				};
+				artists: {
+					external_urls: {
+						spotify: string;
+					};
+					followers: {
+						href: string;
+						total: number;
+					};
+					genres: string[];
+					href: string;
+					id: string;
+					images: [
+						{
+							url: string;
+							height: number;
+							width: number;
+						}
+					];
+					name: string;
+					popularity: number;
+					type: string;
+					uri: string;
+				}[];
+				available_markets: string[];
+				disc_number: number;
+				duration_ms: number;
+				explicit: false;
+				external_ids: {
+					isrc: string;
+					ean: string;
+					upc: string;
+				};
+				external_urls: {
+					spotify: string;
+				};
+				href: string;
+				id: string;
+				is_playable: false;
+				linked_from: {};
+				restrictions: {
+					reason: string;
+				};
+				name: string;
+				popularity: number;
+				preview_url: string;
+				track_number: number;
+				type: string;
+				uri: string;
+				is_local: false;
+			};
+		}[];
+	};
+	type: string;
+	uri: string;
+}

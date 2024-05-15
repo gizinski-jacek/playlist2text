2472ec8b68285b62d6ad1eb626718d74cc9a49be
Jacek Gizinski <jacektrg+github@gmail.com>
2024-05-15 16:46:20 +0200

    Adding functions formatting track list data
    to export to file based on fields selected by user.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,8 +1,10 @@
-export const padNumber = (x: number): string => {
-	return x.toString().padStart(2, '0');
-};
+import { SpotifyTrack } from './types';
 
-export const convertMsToCountdown = (value: number): string => {
+export function padNumber(x: number): string {
+	return x.toString().padStart(2, '0');
+}
+
+export function convertMsToDuration(value: number): string {
 	const ms = value % 1000;
 	value = (value - ms) / 1000;
 	const secs = value % 60;
@@ -10,4 +12,74 @@ export const convertMsToCountdown = (value: number): string => {
 	const mins = value % 60;
 
 	return padNumber(mins) + ':' + padNumber(secs);
-};
+}
+
+export function formatPlaylistToTXT(
+	fields: string[],
+	trackList: SpotifyTrack[]
+) {
+	const txtFields = fields.join(' - ');
+	const txtData = trackList.map((data) => {
+		const array: string[] = [];
+		fields.forEach((field) => {
+			switch (field.toLocaleLowerCase()) {
+				case 'track name':
+					array.push(data.track.name);
+					break;
+				case 'duration':
+					array.push(convertMsToDuration(data.track.duration_ms));
+					break;
+				case 'album name':
+					array.push(data.track.album.name);
+					break;
+				case 'artists':
+					array.push(
+						data.track.album.artists.map((artist) => artist.name).join(', ')
+					);
+					break;
+				case 'album release':
+					array.push(data.track.album.release_date);
+					break;
+				default:
+					break;
+			}
+		});
+		return array.join(' - ');
+	});
+	return [txtFields, ...txtData];
+}
+
+export function formatPlaylistToCSV(
+	fields: string[],
+	trackList: SpotifyTrack[]
+) {
+	const csvFields = '"' + fields.join('","') + '"';
+	const csvData = trackList.map((data) => {
+		const array: string[] = [];
+		fields.forEach((field) => {
+			switch (field.toLocaleLowerCase()) {
+				case 'track name':
+					array.push(data.track.name);
+					break;
+				case 'duration':
+					array.push(convertMsToDuration(data.track.duration_ms));
+					break;
+				case 'album name':
+					array.push(data.track.album.name);
+					break;
+				case 'artists':
+					array.push(
+						data.track.album.artists.map((artist) => artist.name).join(', ')
+					);
+					break;
+				case 'album release':
+					array.push(data.track.album.release_date);
+					break;
+				default:
+					break;
+			}
+		});
+		return '"' + array.join('","') + '"';
+	});
+	return [csvFields, ...csvData];
+}

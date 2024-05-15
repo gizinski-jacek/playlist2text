3cad9f4270f4df2b5517a4d805d1814fdb17ac09
Jacek Gizinski <jacektrg+github@gmail.com>
2024-05-15 16:42:40 +0200

    Adding safelist with colors to user tailwinds dynamic classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			colors: {
+				'custom-primary': 'rgb(var(--color-primary) / <alpha-value>)',
+				'custom-secondary': 'rgb(var(--color-secondary) / <alpha-value>)',
+			},
+		},
+	},
+	plugins: [],
+	safelist: [
+		'text-green-600',
+		'text-red-500',
+		'text-red-600',
+		'text-red-700',
+		'accent-green-600',
+		'accent-red-500',
+		'accent-red-600',
+		'accent-red-700',
+		'decoration-green-600',
+		'decoration-red-500',
+		'decoration-red-600',
+		'decoration-red-700',
+	],
 };
 export default config;

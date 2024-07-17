7ab166143b943066216070a566343b22b1375bcd
Jacek Gizinski <jacektrg+github@gmail.com>
2024-07-17 21:07:50 +0200

    Adding types for spotify albums

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,3 +1,6 @@
+import { PlaylistInfo } from 'youtube-ext';
+import { VideoDetailed } from 'ytmusic-api';
+
 export type SourcesData = {
 	name: string;
 	color: string;
@@ -7,6 +10,7 @@ export type SourcesData = {
 
 export type Sources =
 	| 'spotify'
+	| 'spotify-album'
 	| 'soundcloud'
 	| 'apple-music'
 	| 'youtube'
@@ -46,17 +50,19 @@ export interface SpotifyPlaylistResponse {
 	};
 	public: false;
 	snapshot_id: string;
-	tracks: {
-		href: string;
-		limit: number;
-		next: string;
-		offset: number;
-		previous: string;
-		total: number;
-		items: SpotifyTrack[];
-	};
+	tracks: SpotifyTracksData;
 	type: string;
 	uri: string;
+}
+
+export interface SpotifyTracksData {
+	href: string;
+	limit: number;
+	next: string;
+	offset: number;
+	previous: string;
+	total: number;
+	items: SpotifyTrack[];
 }
 
 export interface SpotifyTrack {
@@ -160,3 +166,103 @@ export interface SpotifyTrack {
 		is_local: false;
 	};
 }
+
+export interface SpotifyAlbumResponse {
+	album_type: string;
+	total_tracks: number;
+	available_markets: string[];
+	external_urls: {
+		spotify: string;
+	};
+	href: string;
+	id: string;
+	images: {
+		url: string;
+		height: number;
+		width: number;
+	}[];
+	name: string;
+	release_date: string;
+	release_date_precision: string;
+	restrictions: {
+		reason: string;
+	};
+	type: string;
+	uri: string;
+	artists: {
+		external_urls: {
+			spotify: string;
+		};
+		href: string;
+		id: string;
+		name: string;
+		type: string;
+		uri: string;
+	}[];
+	tracks: SpotifyAlbumTracksData;
+	copyrights: {
+		text: string;
+		type: string;
+	}[];
+	external_ids: {
+		isrc: string;
+		ean: string;
+		upc: string;
+	};
+	genres: string[];
+	label: string;
+	popularity: number;
+}
+
+export interface SpotifyAlbumTracksData {
+	href: string;
+	limit: number;
+	next: string;
+	offset: number;
+	previous: string;
+	total: number;
+	items: {
+		artists: {
+			external_urls: {
+				spotify: string;
+			};
+			href: string;
+			id: string;
+			name: string;
+			type: string;
+			uri: string;
+		}[];
+		available_markets: string[];
+		disc_number: number;
+		duration_ms: number;
+		explicit: false;
+		external_urls: {
+			spotify: string;
+		};
+		href: string;
+		id: string;
+		is_playable: false;
+		linked_from: {
+			external_urls: {
+				spotify: string;
+			};
+			href: string;
+			id: string;
+			type: string;
+			uri: string;
+		};
+		restrictions: {
+			reason: string;
+		};
+		name: string;
+		preview_url: string;
+		track_number: number;
+		type: string;
+		uri: string;
+		is_local: false;
+	}[];
+}
+
+export type YoutubePlaylistResponse = PlaylistInfo;
+
+export type YTMusicPlaylistResponse = VideoDetailed[];

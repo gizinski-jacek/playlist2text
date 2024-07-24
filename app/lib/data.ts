dcdc4e17a27624f3f42b346082156f0ca007db99
Jacek Gizinski <jacektrg+github@gmail.com>
2024-07-24 20:16:46 +0200

    Adding fixed path for icons.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,41 +1,47 @@
-import { SourcesData } from './types';
+import { SourceList } from './types';
 
-export const sourcesData: SourcesData = [
+export const sourcesData: SourceList = [
 	{
 		name: 'spotify',
 		color: 'green-600',
 		url: 'https://open.spotify.com/search/',
 		urlOption: '/tracks',
+		iconPath: '/spotify.svg',
 	},
 	{
 		name: 'spotify-album',
 		color: 'green-600',
 		url: 'https://open.spotify.com/search/',
 		urlOption: '/tracks',
+		iconPath: '/spotify.svg',
 	},
 	{
 		name: 'soundcloud',
 		color: 'orange-600',
 		url: 'https://soundcloud.com/search?q=',
 		urlOption: '',
+		iconPath: '/soundcloud.svg',
 	},
 	{
 		name: 'youtube',
 		color: 'red-700',
 		url: 'https://www.youtube.com/results?search_query=',
 		urlOption: '',
+		iconPath: '/youtube.svg',
 	},
 	{
 		name: 'youtube-music',
 		color: 'red-600',
 		url: 'https://music.youtube.com/search?q=',
 		urlOption: '',
+		iconPath: '/youtube-music.svg',
 	},
 	{
 		name: 'apple-music',
 		color: 'red-500',
 		url: 'https://music.apple.com/us/search?term=',
 		urlOption: '',
+		iconPath: '/apple-music.svg',
 	},
 ];
 
@@ -44,5 +50,5 @@ export const spotifyFieldNames: string[] = [
 	'Duration',
 	'Album Name',
 	'Artists',
-	'Album Release',
+	'Album Release Date',
 ];
